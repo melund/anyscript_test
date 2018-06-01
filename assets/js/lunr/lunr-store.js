287f056f0499a820135da094bd78c2a6756de704
Travis CI User <travis@example.org>
2018-06-01 10:20:40 +0000

    Update docs after building Travis build 11 of melund/anyscript2
    The docs were built from the branch 'master' against the commit
    0d57cb1ed51951722a80405a37dfa5c4b3e52485.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/melund/anyscript2/jobs/386592409.
    
    The doctr command that was run is
    
        /home/travis/miniconda/envs/doc-build-environment/bin/doctr deploy --built-docs ./_site --deploy-branch-name master --deploy-repo melund/anyscript_test .

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -2,65 +2,65 @@ var store = [{
         "title": "Our new AnyScript landing page",
         "excerpt":"We are building a replacement for the old AnyScript.org page. The site is currently not active but the work is ongoing.                                                                     Old and new AnyScript.org site  The new site links to the new version of the repository site and the wiki site, which are both hosted on GitHub. ","categories": ["News"],
         "tags": ["web site"],
-        "url": "/news/new-anyscript-site/",
-        "teaser":"/assets/images/teaser.jpg"},{
+        "url": "hhttps://melund.github.io/anyscript_test/news/new-anyscript-site/",
+        "teaser":"hhttps://melund.github.io/anyscript_test/assets/images/teaser.jpg"},{
         "title": "Useful AnyScript Macros",
         "excerpt":"AnyBody makes it easy to do musculoskeletal modelling, but some tasks which are simple in normal programming languages (i.e. C, Matlab, Python) can be hard to accomplish in AnyScript. This could be simple tasks like: Creating an abitrary matrix of zeros Adding a constant to single row in a matrix...","categories": ["Tips-n-Tricks"],
         "tags": ["AnyScript"],
-        "url": "/tips-n-tricks/useful-anyscript-macros/",
-        "teaser":"/assets/images/macro_post_teaser.png"},{
+        "url": "hhttps://melund.github.io/anyscript_test/tips-n-tricks/useful-anyscript-macros/",
+        "teaser":"hhttps://melund.github.io/anyscript_test/assets/images/macro_post_teaser.png"},{
         "title": "Creating videos",
         "excerpt":"In this post, I will show a new AnyScript plugin to create awesome videos of yourmusculoskeletal simulations with a single click. Download on GitHub Previously, I often spent hours creating videos for presentations, etc.. Now ittakes me 10 minutes. Besides being easy to use the plugin also supports somewhatcomplicated setups...","categories": ["Tips-n-Tricks"],
         "tags": ["Creating Videos"],
-        "url": "/tips-n-tricks/creating-videos-from-your-simulations/",
-        "teaser":"/assets/images/posts/video_post_teaser.png"},{
+        "url": "hhttps://melund.github.io/anyscript_test/tips-n-tricks/creating-videos-from-your-simulations/",
+        "teaser":"hhttps://melund.github.io/anyscript_test/assets/images/posts/video_post_teaser.png"},{
         "title": "Create Animated GIFs",
         "excerpt":"If a picture is worth a thousand words. What is the value of an animated GIF? If anyone knew the answer to this question it was Eadweard Muybridge who was an 19th century photographer and the grandfather of today’sbiomechanical scientists. He used sequences of photographs to analyze human andanimal motion....","categories": ["Tips-n-Tricks"],
         "tags": ["Creating Videos"],
-        "url": "/tips-n-tricks/create-animated-gifs/",
-        "teaser":"/assets/images/posts/gif-teaser.gif"},{
+        "url": "hhttps://melund.github.io/anyscript_test/tips-n-tricks/create-animated-gifs/",
+        "teaser":"hhttps://melund.github.io/anyscript_test/assets/images/posts/gif-teaser.gif"},{
         "title": "Notepad++ and AnyScript files",
         "excerpt":"In this post, I will show an extension to get syntaxhighlighting and code folding working forNotepad++. This the first post in a series on using external editors with the AnyBodyModeling System. Why another editor? I shall be the first to admit that the editor in the AnyBody Modeling System(AMS) can...","categories": ["Editors"],
         "tags": ["Syntax highlighting"],
-        "url": "/editors/notepad++-and-anyscript/",
-        "teaser":"/assets/images/posts/npp-teaser.png"},{
+        "url": "hhttps://melund.github.io/anyscript_test/editors/notepad++-and-anyscript/",
+        "teaser":"hhttps://melund.github.io/anyscript_test/assets/images/posts/npp-teaser.png"},{
         "title": "AnyMoCap: A model framework motion caputure based models",
         "excerpt":"The AnyMoCap model is an effort to create a simple and unified framework fordoing any kind of mocap analysis with the AnyBody ModelingSystem. This is first post in a series on the AnyMoCap framework. In a series posts Iwill show how to use the frame and presents some of the...","categories": ["AnyMoCap"],
         "tags": ["Motion Capture"],
-        "url": "/anymocap/anymocap-motion-capture-framework/",
-        "teaser":"/assets/images/posts/anymocap_teaser.png"},{
+        "url": "hhttps://melund.github.io/anyscript_test/anymocap/anymocap-motion-capture-framework/",
+        "teaser":"hhttps://melund.github.io/anyscript_test/assets/images/posts/anymocap_teaser.png"},{
         "title": "Calculating the Instantaneous Axis of Rotation",
         "excerpt":"The instantaneous axis of rotation between two bodies is a useful concept inbiomechanics. In this post, we will dig into how to calculate the instantaneousaxis of rotation and show an AnyScript class_template that calculate and displays theaxis between any two reference frames in the AnyBodyModeling System. Before we dive into...","categories": ["Tools"],
         "tags": ["Kinematics"],
-        "url": "/tools/instantaneous-axis-of-rotation/",
-        "teaser":"/assets/images/posts/iaor_teaser.png"},{
+        "url": "hhttps://melund.github.io/anyscript_test/tools/instantaneous-axis-of-rotation/",
+        "teaser":"hhttps://melund.github.io/anyscript_test/assets/images/posts/iaor_teaser.png"},{
         "title": "Add limits to the Range of Motion",
         "excerpt":"In this post, I will show a new AnyScript class template to add range-of-motionlimits to Human body model. The implementation consist of two AnyScript class templates. A highlevel class template RangeOfMotionLimits which makes it easy to addRange-of-motion limits to all the joints on the Musculoskeltal model. Behind the scenes the...","categories": ["tools"],
         "tags": ["kinematics","joints"],
-        "url": "/tools/rom-limits/",
-        "teaser":"/assets/images/posts/rom-limits_teaser.png"},{
+        "url": "hhttps://melund.github.io/anyscript_test/tools/rom-limits/",
+        "teaser":"hhttps://melund.github.io/anyscript_test/assets/images/posts/rom-limits_teaser.png"},{
         "title": "Body Model Configuration App",
         "excerpt":"Graphical user interfaces (GUIs) enables easy and intuitive interaction betweenusers and computer software. But GUIs may also hamper the flexibility and makethe software less powerful. Hence, many powerful software tools are based onscripts, text and code. This is also the case of the AnyBody Modeling System.AnyScript is the language in...","categories": ["Tools"],
         "tags": ["Model configuration","BM switches"],
-        "url": "/tools/body-model-configurator/",
-        "teaser":"/assets/images/posts/BM_Plugin_teaser.JPG"},{
+        "url": "hhttps://melund.github.io/anyscript_test/tools/body-model-configurator/",
+        "teaser":"hhttps://melund.github.io/anyscript_test/assets/images/posts/BM_Plugin_teaser.JPG"},{
         "title": "AnyScript support in Visual Studio Code",
         "excerpt":"Visual Studio Code(VS Code) is one of the newest texteditors to join the group of new powerful source code editors like Atom, Sublime Text and brackets. All of which have become extremely popular in recent years. Visual Studio Code is currently my favorite text editor. It is cross-platform, lightweight, extensible...","categories": ["Editors"],
         "tags": ["Syntax highlighting","Visual Studio Code"],
-        "url": "/editors/anyscript-in-vscode/",
-        "teaser":"/assets/images/posts/vscode-teaser.png"},{
+        "url": "hhttps://melund.github.io/anyscript_test/editors/anyscript-in-vscode/",
+        "teaser":"hhttps://melund.github.io/anyscript_test/assets/images/posts/vscode-teaser.png"},{
         "title": "Freeing the AnyBody Tutorials",
         "excerpt":"Anyone, who works with AnyBody modeling system has at one time used the AnyBodytutorials. They are a valuable resource when learning to use the AnyBodyModeling System and there are more than 15 different tutorials coveringeverything from ‘Getting Started’ tutorials for the new users to very advancedtopics like force dependent kinematics...","categories": ["news"],
         "tags": ["Tutorials","Sphinx"],
-        "url": "/news/freeing-the-anybody-tutorials/",
-        "teaser":"/assets/images/posts/tutorials_teaser.png"},{
+        "url": "hhttps://melund.github.io/anyscript_test/news/freeing-the-anybody-tutorials/",
+        "teaser":"hhttps://melund.github.io/anyscript_test/assets/images/posts/tutorials_teaser.png"},{
         "title": "Getting AnyBody and Numpy to work",
         "excerpt":"With the release of the AnyBody Modeling System 7.1 update, we thought it wastime to make a post about all the improvements in the new model repository. The AnyBody Managed Model Repository (AMMR) version 2.0 marks the culmination ofa huge task to update our existing models and adding new models...","categories": ["news"],
         "tags": ["AMMR","Models"],
-        "url": "/news/Numpy-and-scipy/",
-        "teaser":"/assets/images/posts/ammr2_teaser.png"},{
+        "url": "hhttps://melund.github.io/anyscript_test/news/Numpy-and-scipy/",
+        "teaser":"hhttps://melund.github.io/anyscript_test/assets/images/posts/ammr2_teaser.png"},{
         "title": "What is new AMMR 2.0",
         "excerpt":"Join us for the launch of version 7.1 of our musculoskeletal simulation software - The AnyBody Modeling System, and a new version of the AnyBody Model repository (AMMR 2.0). In this webcast, Morten Enemark Lund, R&amp;D Engineer at AnyBody Technology, will the new software features and the exciting new model...","categories": ["news"],
         "tags": ["AMMR","Models"],
-        "url": "/news/ammr-2.0/",
-        "teaser":"/assets/images/posts/ammr2_teaser.png"}]
+        "url": "hhttps://melund.github.io/anyscript_test/news/ammr-2.0/",
+        "teaser":"hhttps://melund.github.io/anyscript_test/assets/images/posts/ammr2_teaser.png"}]
